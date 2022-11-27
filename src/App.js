ce0e6bf8d476590f25d274b08aedf145b94a7c47
Sanjar872 <Sanjarbek.muxiddinov@gmail.com>
2022-11-27 20:41:54 +0300

    sanjar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Routes, Route} from "react-router-dom"
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home'
+import Menu from './Components/Menu/Menu'
+import Worktime from './Components/WorkTime/Worktime';
+import Info from './Components/Infor/Info';
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Container'>
+      <Navbar/>
+      <Home/>
+      <Menu/>
+      <Worktime/>
+      <Info/>
+      <Footer/>
+
     </div>
   );
 }
---
+++ src/Components/WorkTime/Worktime.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Worktime = () => {
+  return (
+    <div>Worktime</div>
+  )
+}
+
+export default Worktime
