98bf60f0bb2201bc69724b139dd3f42475fe587b
Sanjar872 <Sanjarbek.muxiddinov@gmail.com>
2022-11-27 21:07:44 +0300

    qwer

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Container } from './style'
 
 const Navbar = () => {
   return (
-    <div>
-      navbar
-    </div>
+    <Container>
+      
+    </Container>
   )
 }
 
