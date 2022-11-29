cc910923bd3dfeae00efeae6b2c5ee189b3cd6a0
Sanjar872 <Sanjarbek.muxiddinov@gmail.com>
2022-11-29 10:38:21 +0300

    SanjarAka

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import { Container } from './style'
+import { Brand, Container, Fonimg, Local, Nav, Navdiv } from './style'
+import Smsicons from './images/sms.png'
+import Facebook from './images/facebook.png'
+import Instagram from './images/instagreme.png'
 
 const Navbar = () => {
   return (
     <Container>
-      
+        <Nav>
+          <Navdiv>
+            <ul>
+              <li>About</li>
+              <li>Menu</li>
+              <li>Info</li>
+              <li>Booking</li>
+            </ul>
+          </Navdiv>
+          <Brand>
+            <h1>LATTE CAFE</h1>
+          </Brand>
+
+          <Local>
+              <img src={Smsicons} alt="" />
+              <img src={Facebook} alt="" />
+              <img src={Instagram} alt="" />
+          </Local>
+        </Nav>
+        <Fonimg>
+        </Fonimg>
     </Container>
   )
 }
--- src/Components/Navbar/style.jsx
+++ src/Components/Navbar/style.jsx
@@ -1,5 +1,59 @@
 import styled from "styled-components";
-
+import Navimage from './images/nav-img.png'
 export const Container = styled.div`
-    
+    width: 100%;
+    height: 100vh;
+    padding: 0 4%;
+    background-color:#4A2B33 ;
+
+
+`
+
+export const Nav = styled.div`
+    display: flex;
+    justify-content: space-between;
+    height: 70px;
+    align-items: center;
+`
+
+export const Navdiv = styled.div`
+    width: 250px;
+    ul {
+        display: flex;
+        list-style: none;
+        justify-content: space-between;
+        align-items: center;
+        color:#D27487;
+        font-size: 15px;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 15px;
+        line-height: 24px;
+    }
+
+`
+
+export const Local = styled.div`
+    width: 150px;
+    display: flex;
+    justify-content: space-between;
+`
+
+export const Brand = styled.div`
+    color: #D27487;
+    h1 {
+        font-size: 35px;
+    }
+`
+
+export const Fonimg = styled.div`
+    width: 100%;
+    height: 80vh;
+    background-color: wheat;
+    display: flex;
+    justify-content: center;
+    background-image: url(${Navimage});
+    /* background-position: center; */
+    background-repeat: no-repeat;
+    background-size: cover;
 `
